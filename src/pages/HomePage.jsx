de3247b82feaa8e44d1a140d82a74d57a6b74441
Muhammad Shayan <shayanarif666@gmail.com>
2025-01-24 11:20:58 +0500

    Update Glitch

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,13 +12,88 @@ const HomePage = () => {
 
   return (
     <>
-      <div className={`container-full h-[100vh] w-[100vw] ${isClick ? "active" : ""}`}>
+      <div className={`container-full h-[100vh] w-[100vw] ${isClick ? "active overflow-hidden" : ""}`}>
 
         <Navbar onSideMenuChange={handleSideMenu} />
 
         <div className="main-container">
-          <div className="main relative w-full left-0 z-50 overflow-x-hidden origin-left border-[#ffffff66] transition-all duration-500">
+          <div className={`relative left-0 main w-full z-50 origin-left border-[#ffffff66] transition-all duration-500 h-screen`}>
             <Header bgColor="bg-gradient-to-r from-[#30007d] to-[#000]" />
+            <main id="main_sections" className={`transition-all duration-500 ${isClick ? "opacity-0 pointer-events-none" : "opacity-100"
+              }`}>
+              <section className="bg-sky-950 py-24">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-6">
+                      <h2>Hello</h2>
+                    </div>
+                    <div className="col-6">
+                      <h2>Hello</h2>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <section className="bg-red-950 py-24">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-6">
+                      <h2>Hello</h2>
+                    </div>
+                    <div className="col-6">
+                      <h2>Hello</h2>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <section className="bg-purple-950 py-24">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-6">
+                      <h2>Hello</h2>
+                    </div>
+                    <div className="col-6">
+                      <h2>Hello</h2>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <section className="bg-blue-700 py-24">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-6">
+                      <h2>Hello</h2>
+                    </div>
+                    <div className="col-6">
+                      <h2>Hello</h2>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <section className="bg-green-950 py-24">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-6">
+                      <h2>Hello</h2>
+                    </div>
+                    <div className="col-6">
+                      <h2>Hello</h2>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <section className="bg-orange-600 py-24">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-6">
+                      <h2>Hello</h2>
+                    </div>
+                    <div className="col-6">
+                      <h2>Hello</h2>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </main>
           </div>
 
           <div className="shadow one"></div>
